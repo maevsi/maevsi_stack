b9fb7024de5bac4d096ab39d1558b82715c29576
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-16 17:58:29 +0100

    development/certificates: Use "mkcert"

---
+++ development/certificates/mkcert.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+THIS=$(dirname $(readlink -f "$0"))
+NAME=maevsi
+
+mkcert \
+-cert-file "$THIS/$NAME.crt" \
+-ecdsa \
+-key-file "$THIS/$NAME.key" \
+    $NAME.test \
+    "*.$NAME.test"
