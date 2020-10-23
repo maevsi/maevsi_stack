c3e30caef8e8564db642e8c22823c34ab5db7532
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-10-23 20:15:40 +0200

    refactor(postgraphile): unify quotes

--- src/production/configurations/postgraphile/.postgraphilerc.js
+++ src/production/configurations/postgraphile/.postgraphilerc.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 module.exports = {
     options: {
         connection: fs.readFileSync('/run/secrets/postgraphile_connection', 'utf8'),
-        defaultRole: "maevsi_anonymous",
-        host: "0.0.0.0",
+        defaultRole: 'maevsi_anonymous',
+        host: '0.0.0.0',
         jwtSecret: fs.readFileSync('/run/secrets/postgraphile_jwt-secret', 'utf8'),
-        jwtTokenIdentifier: "maevsi.jwt",
+        jwtTokenIdentifier: 'maevsi.jwt',
         ownerConnection: fs.readFileSync('/run/secrets/postgraphile_owner-connection', 'utf8'),
-        schema: ["maevsi"],
+        schema: ['maevsi'],
         watch: true
     },
 };
