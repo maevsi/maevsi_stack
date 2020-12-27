f25c099a9fb0f81e8e866d591924cf32a0762a6d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-27 03:59:04 +0100

    feat(postgraphile): don't ignore RBAC

--- src/production/configurations/postgraphile/.postgraphilerc.js
+++ src/production/configurations/postgraphile/.postgraphilerc.js
@@ -5,6 +5,7 @@ module.exports = {
         connection: fs.readFileSync('/run/secrets/postgraphile_connection', 'utf8'),
         defaultRole: 'maevsi_anonymous',
         host: '0.0.0.0',
+        ignoreRBAC: false,
         jwtSecret: fs.readFileSync('/run/secrets/postgraphile_jwt-secret', 'utf8'),
         jwtTokenIdentifier: 'maevsi.jwt',
         ownerConnection: fs.readFileSync('/run/secrets/postgraphile_owner-connection', 'utf8'),
