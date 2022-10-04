76b51d439c765a2afdbf84e56db349d6cf06b793
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-10-04 23:48:34 +0200

    feat(certificates): append root certificate

--- src/development/certificates/mkcert.sh
+++ src/development/certificates/mkcert.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 THIS=$(dirname "$(readlink -f "$0")")
 NAME=maevsi
+CERTFILE="$THIS/$NAME.crt"
 
 mkcert \
--cert-file "$THIS/$NAME.crt" \
+-cert-file "$CERTFILE" \
 -ecdsa \
 -key-file "$THIS/$NAME.key" \
     $NAME.test \
     "*.$NAME.test"
+
+cat "$(mkcert -CAROOT)/rootCA.pem" >> "$CERTFILE"
