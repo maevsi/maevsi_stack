52e1cb20645c3914788b0468669b7525a4e3db7d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-10-17 07:07:28 +0200

    Revert "Merge pull request #44 from maevsi/feat/nuxt3"
    This reverts commit bcf797368eccbc1000f0907b4dfc5b99db85a97d.

--- src/development/certificates/mkcert.sh
+++ src/development/certificates/mkcert.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 THIS=$(dirname "$(readlink -f "$0")")
+NAME=maevsi
+CERTFILE="$THIS/$NAME.crt"
 
-create() {
-    NAME="$1"
-    shift
-    CONTENT=$*
+mkcert \
+-cert-file "$CERTFILE" \
+-ecdsa \
+-key-file "$THIS/$NAME.key" \
+    localhost \
+    $NAME.test \
+    "*.$NAME.test"
 
-    path="$THIS/$NAME"
-    certfile="$path.crt"
-    keyfile="$path.key"
-
-    # shellcheck disable=SC2086
-    mkcert \
-        -cert-file "$certfile" \
-        -ecdsa \
-        -key-file "$keyfile" $CONTENT
-
-    cat "$(mkcert -CAROOT)/rootCA.pem" >> "$certfile"
-}
-
-rm "$THIS"/*.key "$THIS"/*.crt
-
-create "localhost" "localhost" "127.0.0.1" "0.0.0.0"
-create "maevsi" "maevsi.test" "www.maevsi.test" "alpha.maevsi.test"
-create "traefik" "*.maevsi.test"
+cat "$(mkcert -CAROOT)/rootCA.pem" >> "$CERTFILE"
