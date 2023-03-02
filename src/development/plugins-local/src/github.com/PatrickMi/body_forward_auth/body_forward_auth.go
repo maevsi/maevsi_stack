5eef3ea51771fe74ec8ed8bbc6cb781cfe9c4503
Patrick Michla <pmichla@gmail.com>
2023-03-02 19:37:05 +0100

    Adds local traefik auth plugin
    Adds the local traefik auth plugin. The plugin connects to http://maevsi:3000/api/auth-proxy. But there are still problems with CORS and the turnstile headers

--- src/development/plugins-local/src/github.com/PatrickMi/body_forward_auth/body_forward_auth.go
+++ src/development/plugins-local/src/github.com/PatrickMi/body_forward_auth/body_forward_auth.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 // Config holds configuration to be passed to the plugin i.e. variables that come from the dockerfile
@@ -30,6 +31,8 @@ type BodyForwardAuth struct {
 
 // New instantiates and returns the required components used to handle an HTTP request
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	log.Printf("The body_forward_auth plugin was instantiated")
+	log.Printf("The authentication URL is: %s", config.AuthUrl)
 	if len(config.AuthUrl) == 0 {
 		return nil, fmt.Errorf("AuthUrl cannot be empty")
 	}
@@ -42,12 +45,16 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 }
 
 func (bfa *BodyForwardAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	log.Printf("The ServeHTTP function was called. Will read the body now.")
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		log.Printf("Error while reading the body: %e\n", err)
 	}
-	log.Printf("The request body is: %s", body)
+	dump, err := httputil.DumpRequest(req, true)
+	log.Printf("The incoming request is: %s", dump)
+	// log.Printf("The request Header is: %s", req.Header)
+	// log.Printf("The request body is: %s", body)
 
 	httpClient := &http.Client{}
 
